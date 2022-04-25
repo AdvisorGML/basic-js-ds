d260beac892c4259cace80fb9a1d192ff94d7f9d
AdvisorGML <advisor@tut.by>
2022-04-25 21:52:42 +0300

    refactor: change formatting of the queue task

--- src/queue.js
+++ src/queue.js
@@ -21,19 +21,14 @@ class Queue {
   }
 
   getUnderlyingList() {
-    // throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-let current = this.head;
-while(current){
-  return current;
-  current=current.next;
-}
-
+    let current = this.head;
+    while (current) {
+      return current;
+      current = current.next;
+    }
   }
 
   enqueue(value) {
-    // throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
     if (this.length === 0) {
       this.head = new ListNode(value)
     } else {
@@ -47,8 +42,6 @@ while(current){
   }
 
   dequeue() {
-    // throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
     let current = this.head;
     this.head = current.next;
     this.length--;
@@ -62,7 +55,7 @@ module.exports = {
 
 const queue = new Queue();
 
-queue.enqueue(1); // adds the element to the queue
-queue.enqueue(3); // adds the element to the queue
-queue.dequeue(); // returns the top element from queue and deletes it, returns 1
-queue.getUnderlyingList() // returns { value: 3, next: null }
+// queue.enqueue(1); // adds the element to the queue
+// queue.enqueue(3); // adds the element to the queue
+// queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+// queue.getUnderlyingList() // returns { value: 3, next: null }
