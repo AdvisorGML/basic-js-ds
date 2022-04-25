999c43f54d612169a61813b8c935be3f3b19627f
AdvisorGML <advisor@tut.by>
2022-04-26 00:31:38 +0300

    feat: add remove-from-list task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,96 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ class List {
+
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  remove(k) {
+    let current = this.head;
+    let index=0;
+    while(current){
+    this.removeEl(this.IndexOf(k));
+    current=current.next
+    }
+  }
+
+  IndexOf(el) {
+    let current = this.head;
+    let index = 0;
+
+    while (current) {
+      if (current.value === el) {
+        return index;
+      }
+      current = current.next;
+      index++;
+    }
+    return -1;
+  }
+  removeEl(pos) {
+    if (pos < 0 || pos >= this.length) {
+      console.log(this.head);
+      return this.head;}
+
+    let current = this.head;
+    if (pos === 0) {
+      this.head = current.next;
+    } else {
+      let prev = null;
+      let index = 0;
+      while (index < pos) {
+        prev = current;
+        current = current.next;
+        index++;
+      }
+      prev.next = current.next;
+    }
+    this.length--;
+    return pos;
+  }
+/////////////////////////////////////////////
+enqueue(value) {
+  if (this.length === 0) {
+    this.head = new ListNode(value)
+  } else {
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = new ListNode(value)
+  }
+  this.length++;
 }
+
+////////////////////////////////////////////
+
+
+}
+
+
+ const list = new List();
+
+function removeKFromList(l, k) {
+console.log(l);
+  list.remove(k);
+}
+
+
 
 module.exports = {
   removeKFromList
 };
+
+
+[3, 1, 2, 3, 4, 5]
+
+list.enqueue(3);
+list.enqueue(1);
+list.enqueue(2);
+list.enqueue(3);
+list.enqueue(4);
+list.enqueue(5);
+removeKFromList(1,3);
--- test/remove-from-list.test.js
+++ test/remove-from-list.test.js
@@ -22,6 +22,7 @@ describe('Remove from list', () => {
   it.optional('should return the list without values equal to k', () => {
     const initial = convertArrayToList([3, 1, 2, 3, 4, 5]);
     const expected = convertArrayToList([1, 2, 4, 5]);
+    // console.log(initial);
     assert.deepEqual(removeKFromList(initial, 3), expected);
   });
 
