bfaaeef96b9ccd7e1afe7189e0e0bf4d5045bbb9
AdvisorGML <advisor@tut.by>
2022-04-26 23:02:51 +0300

    feat: add root,add metods to BST

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,42 +8,59 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.tree = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return (!this.tree) ? null : this.tree
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.tree = addNode(this.tree, data);
+
+    function addNode(current, data) {
+      if (!current) return new Node(data);
+      if (current.data === data) return current;
+
+      if (data < current.data) {
+        current.left = addNode(current.left, data);
+      } else {
+        current.rigth = addNode(current.rigth, data);
+      }
+      return current;
+    }
+    
   }
 
-  has(/* data */) {
+  has(data) {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 
-  find(/* data */) {
+  find(data) {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 
-  remove(/* data */) {
+  remove(data) {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 
   min() {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 
   max() {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 }
 
 module.exports = {
   BinarySearchTree
 };
+
+// const tree = new BinarySearchTree();
+
+// tree.add(20);
+// tree.add(25);
+// tree.add(2);
+// console.log(tree);
